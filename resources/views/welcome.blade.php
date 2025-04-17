7849ef59b903259267ef4f7600d227d1a2ff5ae3
Ellis <ellisbrookes98@gmail.com>
2025-04-17 17:59:44 +0100

    style changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,21 +6,21 @@
     <title>Autistic Led</title>
     @vite('resources/css/app.css')
 </head>
-<body class="bg-gray-100 text-black">
+<body class="bg-gray-100 text-white">
 
-    <nav class="bg-white shadow-md">
+    <nav class="bg-black shadow-md">
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between items-center h-16"> 
                 <div class="flex items-center space-x-4">
-                    <img src="{{ asset('img/logo.jpg') }}" alt="Logo" class="h-12 w-12 object-cover rounded-full">
+                    <img src="{{ asset('img/nav-logo.png') }}" alt="Logo" class="h-12 w-12 object-cover rounded-full">
                 </div>
 
-                <!-- Nav Links -->
                 <div class="hidden sm:flex space-x-8">
-                    <a href="{{ url('/') }}" class="text-gray-700 hover:text-gray-900 font-medium">Home</a>
-                    <a href="{{ url('/') }}" class="text-gray-700 hover:text-gray-900 font-medium">About</a>
-                    <a href="{{ url('/') }}" class="text-gray-700 hover:text-gray-900 font-medium">Contact</a>
-                    <a href="{{ url('/') }}" class="text-gray-700 hover:text-gray-900 font-medium">What We Offer</a>
+                    <a href="{{ url('/') }}" class="text-white hover:text-yellow-600 hover:underline font-medium">Home</a>
+                    <a href="{{ url('/') }}" class="text-white hover:text-yellow-600 hover:underline font-medium">About</a>
+                    <a href="{{ url('/') }}" class="text-white hover:text-yellow-600 hover:underline font-medium">Contact</a>
+                    <a href="{{ url('/') }}" class="text-white hover:text-yellow-600 hover:underline font-medium">What We Offer</a>
+                    <a href="{{ url('/') }}" class="text-white hover:text-yellow-600 hover:underline font-medium">Autistic Led Directory</a>
                 </div>
             </div>
         </div>
@@ -28,7 +28,7 @@
 
     <main class="py-8 flex justify-center">
         @yield('content')
-        <img src="{{ asset('img/logo.jpg') }}" alt="Hero Image" class="mx-auto mt-8 h-60 w-auto">
+        <img src="{{ asset('img/logo.png') }}" alt="Hero Image" class="mx-auto mt-8 h-60 w-auto">
     </main>
 
 </body>
