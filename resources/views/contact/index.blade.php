1059ed1e761aff356c783a55d4fe058589b4aff5
Ellis <ellisbrookes98@gmail.com>
2025-04-23 20:01:12 +0100

    cleanup

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -8,24 +8,29 @@
 </head>
 <body class="text-white bg-gray-800">
 
-    <!-- Navbar -->
     <nav class="bg-gray-900 shadow-md">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
-            <a href="{{ url('/') }}">
-                <img src="{{ asset('img/nav-logo.png') }}" alt="Logo" class="h-12 w-12 object-cover rounded-full">
-            </a>
-            <div class="hidden sm:flex space-x-8">
-                <a href="{{ url('/') }}" class="text-white hover:text-yellow-600">Home</a>
-                <a href="{{ url('/about') }}" class="text-white hover:text-yellow-600">About</a>
-                <a href="{{ route('contact') }}" class="text-white hover:text-yellow-600">Contact</a>
-                <a href="{{ url('/services') }}" class="text-white hover:text-yellow-600">What We Offer</a>
-                <a href="{{ url('/directory') }}" class="text-white hover:text-yellow-600">Autistic Led Directory</a>
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="flex justify-between items-center h-16">
+                <div class="flex items-center space-x-4">
+                    <a href="/">
+                        <img src="{{ asset('img/nav-logo.png') }}" alt="Logo" class="h-12 w-12 object-cover rounded-full">
+                    </a>
+                </div>
+                <div class="hidden sm:flex space-x-8">
+                    <a href="{{ url('/') }}" class="text-white hover:text-yellow-600 hover:underline font-medium">Home</a>
+                    <a href="{{ url('/') }}" class="text-white hover:text-yellow-600 hover:underline font-medium">About</a>
+                    <a href="{{ route('contact') }}" class="text-white hover:text-yellow-600 hover:underline font-medium">Contact</a>
+                    <a href="{{ url('/') }}" class="text-white hover:text-yellow-600 hover:underline font-medium">What We Offer</a>
+                    <a href="{{ url('/') }}" class="text-white hover:text-yellow-600 hover:underline font-medium">Autistic Led Directory</a>
+                </div>
+                <div class="sm:hidden flex items-center">
+                    <button id="hamburger-icon" class="text-white focus:outline-none">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
-            <button id="hamburger-icon" class="sm:hidden text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
-                </svg>
-            </button>
         </div>
         <div class="border-t-2 border-white"></div>
     </nav>
