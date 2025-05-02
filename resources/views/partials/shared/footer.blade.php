3d99e65f1d0281804f4c4db70cba63380df49b15
Ellis Brookes <ellisbrookes98@gmail.com>
2025-05-02 21:50:15 +0100

    updated link styling

--- resources/views/partials/shared/footer.blade.php
+++ resources/views/partials/shared/footer.blade.php
@@ -4,11 +4,11 @@
             <div>
                 <h2 class="text-lg font-semibold mb-4">Useful Links</h2>
                 <ul class="space-y-2">
-                    <li><a href="/" class="hover:underline hover:decoration-yellow-500 hover:decoration-wavy hover:decoration-2">Home</a></li>
                     <li><a href="{{ url('about') }}" class="hover:underline hover:decoration-yellow-500 hover:decoration-wavy hover:decoration-2">About</a></li>
-                    <li><a href="{{ url('contact') }}" class="hover:underline hover:decoration-yellow-500 hover:decoration-wavy hover:decoration-2">Contact</a></li>
                     <li><a href="{{ url('services') }}" class="hover:underline hover:decoration-yellow-500 hover:decoration-wavy hover:decoration-2">Services</a></li>
                     <li><a href="{{ url('active_directory') }}" class="hover:underline hover:decoration-yellow-500 hover:decoration-wavy hover:decoration-2">Active Directory</a></li>
+                    <li><a href="{{ url('contact') }}" class="hover:underline hover:decoration-yellow-500 hover:decoration-wavy hover:decoration-2">Contact</a></li>
+                    <li><a href="{{ url('login') }}" class="hover:underline hover:decoration-yellow-500 hover:decoration-wavy hover:decoration-2">Login</a></li>
                 </ul>
             </div>
             <div>
@@ -26,7 +26,7 @@
             &copy; {{ date('Y') }} Autistic Led. All rights reserved.
             <div class="mt-2 text-white">
                 Made with <span class="text-red-500">♥</span> by 
-                <a href="https://ebrookes.dev" target="_blank" class="text-yellow-500 hover:underline font-medium">
+                <a href="https://ebrookes.dev" target="_blank" class="hover:underline hover:decoration-yellow-500 hover:decoration-wavy hover:decoration-2">
                     Ellis Development
                 </a>
             </div>
