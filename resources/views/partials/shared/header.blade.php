4de7fcd816884d79e5bdfa29f7581ea7f7333822
Ellis Marcus Brookes <ellisbrookes98@gmail.com>
2025-10-30 21:11:59 +0000

    move over to ruby on rails (#12)

--- resources/views/partials/shared/header.blade.php
+++
@@ -1,7 +0,0 @@
-<section class="bg-gray-900 flex flex-col items-center p-4 border-b-2 border-white">
-    <img src="{{ asset('img/logo.png') }}" alt="Hero Logo" class="h-34">
-
-    <h1 class="text-white text-lg sm:text-xl tracking-wide my-4">
-        The journey may vary but it is always Autistic Led
-    </h1>
-</section>
