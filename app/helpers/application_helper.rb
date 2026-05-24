01b99be05070c1f347ab8e081d48d31a08ce905a
Ellis <ellisbrookes98@gmail.com>
2026-05-24 23:21:53 +0100

    Add safe_http_url helper and update website link rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def safe_http_url(value)
+    return if value.blank?
+
+    uri = URI.parse(value)
+    return unless uri.is_a?(URI::HTTP) && uri.host.present?
+
+    uri.to_s
+  rescue URI::InvalidURIError
+    nil
+  end
 end
--- app/models/directory_listing.rb
+++ app/models/directory_listing.rb
@@ -3,6 +3,12 @@ class DirectoryListing < ApplicationRecord
   has_many_attached :images
 
   validates :name, :listing_type, :location, :description, presence: true
+  validates :website_url,
+    format: {
+      with: URI::DEFAULT_PARSER.make_regexp(["http", "https"]),
+      message: "must be a valid HTTP(S) URL"
+    },
+    allow_blank: true
 
   def support_tags
     supports.to_s.split(/[\n,]/).map(&:strip).reject(&:blank?)
