fa514a20b37baf6b3fd14b98d46363490c79b9bb
Ellis <ellisbrookes98@gmail.com>
2026-06-25 10:26:22 +0100

    Update schema version and modify location column in directory_listings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_31_114500) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_25_102318) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -83,7 +83,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_31_114500) do
     t.text "description", null: false
     t.text "labels"
     t.string "listing_type", null: false
-    t.string "location", null: true
+    t.string "location"
     t.string "name", null: false
     t.text "supports"
     t.datetime "updated_at", null: false
